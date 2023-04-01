45417f65e80278e348bb428ba7e4c609389bb525
jdborn7 <115047956+jdborn7@users.noreply.github.com>
2023-03-31 18:23:21 -0700

    Update What are the Primary Majors of Vets and Active Duty at the UofA.R

--- What are the Primary Majors of Vets and Active Duty at the UofA.R
+++ What are the Primary Majors of Vets and Active Duty at the UofA.R
@@ -34,24 +34,43 @@ colnames(survey_data) = c('start_date',
                           'gender',
                           'ethnicity')
 
-ggplot(survey_data,aes(service_branch, primary_major, color = age))+
+p <- ggplot(survey_data,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10, position=position_jitter(width=0, height=0.2))+
-  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",x = "Service Branch", y = "Primary Major")
+  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",
+       x = "Service Branch", y = "Primary Major", fill = "Age Groups")+
+  theme_fivethirtyeight()+
+  theme(axis.title=element_text(), plot.title = element_text(hjust = 0.5))
+
+ggplotly(p)
 
 m <- filter(survey_data, gender == "M")
 f <- filter(survey_data, gender == "F")
 g <- filter(survey_data, gender == "G" | gender == "N")
 
-ggplot(m,aes(service_branch, primary_major, color = age))+
+p2 <- ggplot(m,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10, position=position_jitter(width=0, height=0.2))+
-  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",x = "Service Branch", y = "Primary Major")
+  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",
+       x = "Service Branch", y = "Primary Major", fill = "Age Groups")+
+  theme_fivethirtyeight()+
+  theme(axis.title=element_text(), plot.title = element_text(hjust = 0.5))
 
-ggplot(f,aes(service_branch, primary_major, color = age))+
+ggplotly(p2)
+
+p3 <- ggplot(f,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10, position=position_jitter(width=0, height=0.1))+
-  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",x = "Service Branch", y = "Primary Major")
+  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",
+       x = "Service Branch", y = "Primary Major", fill = "Age Groups")+
+  theme_fivethirtyeight()+
+  theme(axis.title=element_text(), plot.title = element_text(hjust = 0.5))
 
-ggplot(g,aes(service_branch, primary_major, color = age))+
+ggplotly(p3)
+
+p4 <- ggplot(g,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10)+
-  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",x = "Service Branch", y = "Primary Major")
+  labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",
+       x = "Service Branch", y = "Primary Major", fill = "Age Groups")+
+  theme_fivethirtyeight()+
+  theme(axis.title=element_text(), plot.title = element_text(hjust = 0.5))
 
+ggplotly(p4)
 
