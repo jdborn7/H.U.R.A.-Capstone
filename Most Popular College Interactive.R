7bc74aee158b417a6366a1cd113aba19ed36c3b7
jdborn7 <115047956+jdborn7@users.noreply.github.com>
2023-04-06 14:13:56 -0700

    Update Most Popular College Interactive.R

--- Most Popular College Interactive.R
+++ Most Popular College Interactive.R
@@ -1,12 +1,29 @@
+#Jade Born
+#This code was created to produce a wordcloud of the most
+#popular colleges for the Veterans and Active Duty members
+#attending the Unversity of Arizona. It is intended to be
+#used as a introductory graph to the information that we 
+# used to answer our research questions.
+
+
+#call in libraries
 library(tidyverse)
 library(wordcloud2)
+library(plotly)
+library(ggplot2)
 
-choices = read.csv("C:/Users/JDBOR/OneDrive/Desktop/Stats labs/capstone/2023 Higher Education Survey_March 19, 2023_choices - Sheet1.csv")
+#read in the survey data
+survey_data = read.csv("C:/Users/JDBOR/OneDrive/Desktop/Stats labs/capstone/Final_data_choices.csv")
 
-df = data.frame(colleges = choices$Q4)
+#translate just the colleges answer into a dataframe
+df = data.frame(colleges = survey_data$Q4)
 
+#takes that dataframe and adds a frequency column to allow for the amount of people in that college
+#to be counted
 feq = df %>%
          count(colleges)
 
-wordcloud2(feq)
+#produces a word cloud on a grid sized 5
+wordcloud2(feq,size=1, color = "random-dark", gridSize = 5)
+
 
