bdd2ef3b3e48b2c17345b55efd4c786be3ce566e
jdborn7 <115047956+jdborn7@users.noreply.github.com>
2023-04-07 16:42:33 -0700

    Update Does My Career Relate to Prior Fields of Work.R

--- Does My Career Relate to Prior Fields of Work.R
+++ Does My Career Relate to Prior Fields of Work.R
@@ -1,11 +1,20 @@
+#Jade Born
+#This graph is to showcase the whether or not 
+#the major that the Veteran or Active Duty Member
+#is affected by what their career is/was in their
+#time in the military. It is a look at factors that
+#could answer our 3rd Research question
+
+#call libraries
 library(tidyverse)
 library(plotly)
 library(ggplot2)
 library(ggthemes)
 
+#read in the survey data
 survey_data = read.csv("C:/Users/JDBOR/OneDrive/Desktop/Stats labs/capstone/Final_data_choices.csv")
 
-
+#clean up the survey data to make it easier to graph
 survey_data <- survey_data[,-22]
 survey_data <- survey_data[,-23]
 survey_data <- survey_data[,-23]
@@ -34,7 +43,8 @@ colnames(survey_data) = c('start_date',
                           'ethnicity')
 
 
-
+#create a horizontal bar graph by service branch to see the responses made
+#by the survey participents.
 ggplot(survey_data)+
   geom_bar(mapping = aes(y = career_related, fill = career_related), 
            show.legend = FALSE)+
