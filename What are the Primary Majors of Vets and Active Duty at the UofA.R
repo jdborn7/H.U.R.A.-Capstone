7520e01a7237f7576d22168496db4464a6c97ec5
jdborn7 <115047956+jdborn7@users.noreply.github.com>
2023-04-07 16:48:56 -0700

    Update What are the Primary Majors of Vets and Active Duty at the UofA.R

--- What are the Primary Majors of Vets and Active Duty at the UofA.R
+++ What are the Primary Majors of Vets and Active Duty at the UofA.R
@@ -1,12 +1,23 @@
+#Jade Born
+#This code creates a graph to show the majors that are most
+#popular by the branches. It goes along with the bar graph 
+#that shows if majors are similar to prior careers. It is 
+#a way of understanding if service branches does effect what 
+#major is taken and if the University of Arizona offers majors
+#that Veterans and active duty members want to take.
+
+#call libraries
 library(tidyverse)
 library(wordcloud2)
 library(ggwordcloud)
 library(plotly)
 library(ggplot2)
 
+
+#read in survey data
 survey_data = read.csv("C:/Users/JDBOR/OneDrive/Desktop/Stats labs/capstone/2023 Higher Education Survey_March 19, 2023_choices - Sheet1.csv")
 
-
+#clean survey data for easier graphing work
 survey_data <- survey_data[,-22]
 survey_data <- survey_data[,-23]
 survey_data <- survey_data[,-23]
@@ -34,6 +45,8 @@ colnames(survey_data) = c('start_date',
                           'gender',
                           'ethnicity')
 
+#create the scatter plot of the majors and service branchs and save 
+#it to a variable
 p <- ggplot(survey_data,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10, position=position_jitter(width=0, height=0.2))+
   labs(title = "What are the Primary Majors of Vets and Active Duty at the UofA?",
@@ -41,11 +54,16 @@ p <- ggplot(survey_data,aes(service_branch, primary_major, color = age))+
   theme_fivethirtyeight()+
   theme(axis.title=element_text(), plot.title = element_text(hjust = 0.5))
 
+#call the variable the plot was saved to and creates an interactive 
+#version of the graph.
 ggplotly(p)
 
+
+#extra graphs seperated male, female and those who do not identify with those genders.
+#this helps use look at even more centralized versions of the first graph
 m <- filter(survey_data, gender == "M")
 f <- filter(survey_data, gender == "F")
-g <- filter(survey_data, gender == "G" | gender == "N")
+g <- filter(survey_data, gender != "M" | gender != "F")
 
 p2 <- ggplot(m,aes(service_branch, primary_major, color = age))+
   geom_point(alpha = 0.3, size = 10, position=position_jitter(width=0, height=0.2))+
